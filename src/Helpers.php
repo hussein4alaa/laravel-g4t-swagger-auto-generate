2ec6058bb15042bc2b6c33623f9cbe502badd11e
Waad Mawlood <w12345678996392@gmail.com>
2023-08-16 15:41:30 +0300

    added dump support laravel 7,8,9,10 and optimize code of Helpers traits

--- src/Helpers.php
+++ src/Helpers.php
@@ -2,114 +2,174 @@
 
 namespace G4T\Swagger;
 
+use Illuminate\Routing\Route;
+use Illuminate\Support\Str;
 use ReflectionException;
 use ReflectionMethod;
-use Illuminate\Support\Str;
 
 trait Helpers
 {
-
-    public function getControllerName($action)
+    /**
+     * Get pure name of controller
+     *
+     * @param string $action
+     * @return array<string>|string
+     */
+    public function getControllerName(string $action)
     {
-        $segments = explode('\\', $action);
-        $controller = end($segments);
+        $controller = class_basename($action);
+        $controller = Str::contains($controller, '@') ? Str::beforeLast($controller, '@') : $controller;
+        $controller = Str::replace(['Controller', 'controller'], '', $controller);
 
-        if (strpos($controller, '@') !== false) {
-            $controller = explode('@', $controller)[0];
-        }
-
-        $controller = str_replace(['Controller', 'controller'], '', $controller);
         return $controller;
     }
 
-    public function isApiRoute($route)
+    /**
+     * Check if the route is an API request
+     *
+     * @param Route $route
+     * @return bool
+     */
+    public function isApiRoute(Route $route)
     {
         return in_array('api', $route->middleware());
     }
 
-
-    public function getRouteName($route, $prefix)
+    /**
+     * Name of route after prefix
+     *
+     * @param string $route
+     * @param string $prefix
+     * @return string
+     */
+    public function getRouteName(string $route, string $prefix)
     {
-        $escapedPrefix = preg_quote($prefix, '/');
-        $regex = "/{$escapedPrefix}\/([^\/]+)/";
-        if (preg_match($regex, $route, $matches)) {
-            return $matches[1];
+        if (Str::startsWith($route, $prefix . '/')) {
+            $trimmedRoute = Str::after($route, $prefix . '/');
+            $routeName = Str::before($trimmedRoute, '/');
+            return $routeName;
         }
+
         return 'unknown';
     }
 
-
-    public function generateOperationId($uri, $method)
+    /**
+     * Generate Operation Id
+     *
+     * @param string $uri
+     * @param string $method
+     * @return array<string>|string
+     */
+    public function generateOperationId(string $uri, string $method)
     {
-        $operationId = str_replace(['/', '[', ']'], '_', $uri) . '_' . $method;
-        $operationId = str_replace(['{', '}'], '_', $operationId);
-
-        return $operationId;
+        return Str::replace(['/', '[', ']', '{', '}'], ['_', '_', '_', '_', '_'], $uri . '_' . $method);
     }
 
-
-
+    /**
+     * Get validation of request for controller's method
+     *
+     * @param string $controllerMethod
+     * @return mixed
+     */
     public function getRequestClassName(string $controllerMethod)
     {
-        if ($controllerMethod !== 'Closure') {
-            list($class, $method) = explode('@', $controllerMethod);
-            try {
-                $reflection = new ReflectionMethod($class, $method);
-                $parameters = $reflection->getParameters() ?? [];
-                foreach ($parameters as $parameter) {
-                    $typeHint = $parameter ? $parameter->getType() : null;
-                    if ($typeHint && !$typeHint->isBuiltin()) {
-                        try {
-                            $request = $typeHint->getName();
-                            $request = new $request();
-                            return $request->rules();
-                        } catch (\Throwable $th) {
-                        }
-                    }
-                }
-            } catch (ReflectionException $e) {
-                return [];
-            }
+        if ($controllerMethod === 'Closure') {
             return null;
         }
+
+        list($class, $method) = explode('@', $controllerMethod);
+
+        try {
+            $parameters = $this->getMethodParameters($class, $method);
+
+            foreach ($parameters as $parameter) {
+                $typeHint = $parameter->getType();
+
+                if ($typeHint && !$typeHint->isBuiltin()) {
+                    try {
+                        $request = $typeHint->getName();
+                        return $this->getRequestRules($request);
+                    } catch (\Throwable $th) {
+                    }
+                }
+            }
+        } catch (ReflectionException $e) {
+        }
+
+        return [];
     }
 
+    /**
+     * Get parameters of method
+     *
+     * @param string $class
+     * @param string $method
+     * @return array<\ReflectionParameter>
+     */
+    private function getMethodParameters(string $class, string $method)
+    {
+        $reflection = new ReflectionMethod($class, $method);
+        return $reflection->getParameters() ?? [];
+    }
 
-    public function schemaName($action)
+    /**
+     * Get request rules of controller's method
+     *
+     * @param string $requestClass
+     * @return mixed
+     */
+    private function getRequestRules(string $requestClass)
+    {
+        $request = new $requestClass();
+        return $request->rules();
+    }
+
+    /**
+     * Get schema Name of controller and method
+     *
+     * @param string $action
+     * @return string
+     */
+    public function schemaName(string $action)
     {
         $className = class_basename($action);
-        $className = str_replace('Controller', '', $className);
-        $className = strstr($className, '@', true);
+        $className = Str::replaceLast('Controller', '', $className);
+        $className = Str::before($className, '@');
         $methodName = Str::afterLast($action, '@');
         $modifiedClassName = Str::studly(ucfirst($className) . ucfirst($methodName));
+
         return $modifiedClassName;
     }
 
-
-    public function checkIfQueryParamRequiredOrNot($params)
+    /**
+     * Check if query parameter is required or not
+     *
+     * @param array|string $params
+     * @return bool
+     */
+    public function checkIfQueryParamRequiredOrNot(mixed $params)
     {
-        $required = false;
         if (!is_array($params)) {
             $params = explode('|', $params);
         }
-        foreach ($params as $param) {
-            if ($param == 'required') {
-                $required = true;
-                break;
-            }
-        }
-        return $required;
+
+        return in_array('required', $params);
     }
 
-    public function formatParams($validations, $route)
+    /**
+     * Get format Parammters of controller's method
+     *
+     * @param mixed $validations
+     * @param Route $route
+     * @return array<array>
+     */
+    public function formatParams(mixed $validations, Route $route)
     {
         $method = $route->methods();
         $params_list = [];
-        if (
-            in_array($method, ['PUT', 'put', 'Put'])
-            or is_array($method) && in_array('GET', $method)
-        ) {
-            if (!is_null($validations)) {
+
+        if (in_array('PUT', $method) || in_array('GET', $method)) {
+            if (filled($validations)) {
                 foreach ($validations as $key => $param) {
                     $params_list[] = [
                         "name" => $key,
@@ -139,19 +199,17 @@ trait Helpers
         return $params_list;
     }
 
-
-
-    public function checkIfTokenIsRequired($route)
+    /**
+     * Check if token is required or not
+     *
+     * @param Route $route
+     * @return bool
+     */
+    public function checkIfTokenIsRequired(Route $route)
     {
         $middlewares = $route->gatherMiddleware();
         $authMiddlewares = config('swagger.auth_middlewares');
 
-        foreach ($middlewares as $middleware) {
-            if (in_array($middleware, $authMiddlewares)) {
-                return true;
-            }
-        }
-
-        return false;
+        return count(array_intersect($middlewares, $authMiddlewares)) > 0;
     }
 }

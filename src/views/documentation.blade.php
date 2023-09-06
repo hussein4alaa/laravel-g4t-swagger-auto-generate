848dfdeb7ee109cd0793384fc00fc19ed07a8a16
HusseinAlaa <hussein4alaa@gmail.com>
2023-09-06 15:30:29 +0300

    Update documentation.blade.php

--- src/views/documentation.blade.php
+++ src/views/documentation.blade.php
@@ -12,25 +12,125 @@
 
 <body>
 
-    <div class="openapi-icon">
-        <a href="{{ route('swagger.json') }}" class="link-swagger">OpenAPI 3.0.3</a>
+    <div class="openapi-nav">
+        <ul class="nav-ul">
+            <li>
+                <a target="_blank" href="{{ route('swagger.json') }}" class="link-swagger">Download Swagger File</a>
+                <a onclick="openModal()" class="link-swagger">Set Global Authorization</a>
+            </li>
+        </ul>
     </div>
+
+    <div class="global-token-container" id="myModal">
+        <span class="global-title">Set Global Authorization</span>
+        <hr>
+        <div id="key-cont" class="authTokenInput-cont">
+            <label for="authTokenKey">Auth Key</label>
+            <input type="text" class="global-token" id="authTokenKey" placeholder="Enter Authorization Key"
+                value="Authorization">
+        </div>
+        <div id="value-cont" class="authTokenInput-cont">
+            <label for="authTokenKey">Auth Value</label>
+            <input type="text" class="global-token" id="authTokenInput" placeholder="Enter Authorization Token">
+        </div>
+
+        <div class="action-btns">
+            <button class="btn btn-auth" onclick="saveAuthToken()">Authorize</button>
+            <button class="btn btn-logout" onclick="logoutToken()">Logout</button>
+            <button class="btn btn-logout" onclick="closeModal()">Close</button>
+        </div>
+    </div>
+
+    <div id="overlay"></div>
+
+
     <div id="swagger-ui"></div>
 
     <script>
-        var data = @json($response);
-        const jsonContent = jsyaml.load(data);
-        renderSwaggerUI(data);
-        function renderSwaggerUI(jsonContent) {
-            const ui = SwaggerUIBundle({
-                spec: jsonContent,
-                dom_id: "#swagger-ui",
-                deepLinking: true,
-                presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
-                plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-                layout: "BaseLayout",
-            });
+        function openModal() {
+            const modal = document.getElementById('myModal');
+            const overlay = document.getElementById('overlay');
+            modal.style.display = 'block';
+            overlay.style.display = 'block';
         }
+
+        function closeModal() {
+            const modal = document.getElementById('myModal');
+            const overlay = document.getElementById('overlay');
+            modal.style.display = 'none';
+            overlay.style.display = 'none';
+        }
+
+        // Function to close the modal
+        // function closeModal() {
+        //     const modal = document.getElementById('myModal');
+        //     const overlay = document.getElementById('modalOverlay');
+
+        //     modal.style.display = 'none';
+        //     overlay.style.display = 'none';
+        // }
+
+
+        // Function to save the authorization token to a cookie
+        function saveAuthToken() {
+            const authToken = document.getElementById('authTokenInput').value;
+            const authTokenKey = document.getElementById('authTokenKey').value;
+            localStorage.setItem('authorizationToken', authToken);
+            localStorage.setItem('authorizationKey', authTokenKey);
+            document.getElementById('key-cont').style.display = 'none';
+            document.getElementById('value-cont').style.display = 'none';
+            initializeSwaggerUI();
+        }
+
+        function logoutToken() {
+            localStorage.removeItem('authorizationToken');
+            localStorage.removeItem('authorizationKey');
+            document.getElementById('key-cont').style.display = 'inline';
+            document.getElementById('value-cont').style.display = 'inline';
+            initializeSwaggerUI();
+        }
+
+        function getAuthTokenFromLocalStorage() {
+            return localStorage.getItem('authorizationToken');
+        }
+
+        function getAuthKeyFromLocalStorage() {
+            return localStorage.getItem('authorizationKey');
+        }
+
+
+        function initializeSwaggerUI() {
+            const authToken = getAuthTokenFromLocalStorage();
+            const authKey = getAuthKeyFromLocalStorage();
+            var data = @json($response);
+            const jsonContent = jsyaml.load(data);
+            renderSwaggerUI(data);
+
+            function renderSwaggerUI(jsonContent) {
+                const ui = SwaggerUIBundle({
+                    spec: jsonContent,
+                    dom_id: "#swagger-ui",
+                    deepLinking: true,
+                    presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
+                    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+                    layout: "BaseLayout",
+                    requestInterceptor: (request) => {
+                        if (authToken && authKey) {
+                            request.headers[authKey] = `Bearer ${authToken}`;
+                        }
+                        return request;
+                    }
+
+                });
+            }
+        }
+        $(document).ready(function() {
+            initializeSwaggerUI();
+            if (getAuthTokenFromLocalStorage()) {
+                document.getElementById('key-cont').style.display = 'none';
+                document.getElementById('value-cont').style.display = 'none';
+            }
+        });
     </script>
 </body>
 
